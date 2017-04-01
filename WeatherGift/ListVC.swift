4ef20ef0bdf9cabf298b26d1ab71bf97ec536a4a
Patrick Calabrese <PatrickCalabrese@Patricks-MacBook-Pro.local>
2017-04-01 16:14:25 -0400

    Through 7.9

--- WeatherGift/ListVC.swift
+++ WeatherGift/ListVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GooglePlaces
 
 class ListVC: UIViewController {
 
@@ -51,6 +52,12 @@ class ListVC: UIViewController {
             editBarButton.title = "Done"
             addBarButton.isEnabled = false
         }
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        present(autocompleteController, animated: true, completion: nil)
     }
     
 
@@ -110,6 +117,38 @@ extension ListVC: UITableViewDelegate, UITableViewDataSource {
         } else {
             return proposedDestinationIndexPath
         }
+    }
+    
+}
+
+
+extension ListVC: GMSAutocompleteViewControllerDelegate {
+    
+    // Handle the user's selection.
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        print("Place name: \(place.name)")
+        print("Place address: \(place.formattedAddress)")
+        print("Place attributions: \(place.attributions)")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        // TODO: handle the error.
+        print("Error: ", error.localizedDescription)
+    }
+    
+    // User canceled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // Turn the network activity indicator on and off again.
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
     }
     
 }
